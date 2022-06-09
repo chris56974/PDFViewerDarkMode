748c93aabe63ebe51e544160e78c1359715ed09f
Chris Straka <skylake112@outlook.com>
2022-06-09 13:40:04 -0600

    touch up readme, make div cover full screen

--- content-script.js
+++ content-script.js
@@ -1,26 +1,12 @@
-let darkPdfContainerCss = `
+let darkPdfDivStyles = `
   pointer-events: none;
   width: 100vw;
-  height: calc(100vh - 32px);
-  padding-top: 32px;
-  margin: 0 auto;
-`;
-
-let darkPdfCss = `
-  pointer-events: none;
-  width: 1019px;
-  height: 100%;
-  margin: 0 auto;
+  height: 100vh;
   background-color: white;
   mix-blend-mode: difference;
   z-index: 1; 
 `;
 
-darkPdfContainer = document.createElement('div');
-darkPdf = document.createElement('div');
-
-darkPdfContainer.setAttribute("style", darkPdfContainerCss);
-darkPdf.setAttribute("style", darkPdfCss);
-
-document.body.appendChild(darkPdfContainer)
-darkPdfContainer.appendChild(darkPdf)
+let darkPdfDiv = document.createElement('div');
+darkPdfDiv.setAttribute("style", darkPdfDivStyles);
+document.body.appendChild(darkPdfDiv)
